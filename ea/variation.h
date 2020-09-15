efae1cc8c67b13897f8f9db84dd3705d1f8ea40d
Tomasz Tarkowski <Tomasz.Tarkowski@fuw.edu.pl>
2020-09-15 14:27:54 +0200

    iterative_recombination & arithmetic_recombination.

--- ea/variation.h
+++ ea/variation.h
@@ -1,6 +1,7 @@
 #ifndef LIBBEAR_EA_VARIATION_H
 #define LIBBEAR_EA_VARIATION_H
 
+#include <stdexcept>
 #include <libbear/core/random.h>
 #include <libbear/ea/elements.h>
 #include <libbear/ea/genotype.h>
@@ -64,6 +65,35 @@ namespace libbear {
     const recombination_fn recombine_;
   };
   
+  template<typename T>
+  class iterative_recombination {
+    using fn = std::function<T(T, T)>;
+
+  public:
+    explicit iterative_recombination(const fn& f) : f_{f} {}
+
+    population operator()(const genotype& g0, const genotype& g1) const {
+      if (g0.size() != g1.size()) {
+        throw std::logic_error{"iterative_recombination: size mismatch"};
+      }
+      genotype g_res{g0};
+      for (std::size_t i = 0; i < g0.size(); ++i) {
+        g_res[i]->value<T>(f_(g0[i]->value<T>(), g1[i]->value<T>()));
+      }
+      return population{g_res};
+    }
+
+  private:
+    fn f_;
+  };
+
+  template<typename T>
+  population arithmetic_recombination(const genotype& g0, const genotype& g1) {
+    return iterative_recombination<T>{
+      [](T a, T b) { return 0.5 * a + 0.5 * b; }
+    }(g0, g1);
+  }
+
 } // namespace libbear
 
 #endif // LIBBEAR_EA_VARIATION_H
