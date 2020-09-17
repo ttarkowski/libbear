8a274272b322256656f2c3df5db1c718dd7da4b1
Tomasz Tarkowski <Tomasz.Tarkowski@fuw.edu.pl>
2020-09-17 15:25:18 +0200

    iterative_recombination_on_range

--- ea/variation.h
+++ ea/variation.h
@@ -1,7 +1,10 @@
 #ifndef LIBBEAR_EA_VARIATION_H
 #define LIBBEAR_EA_VARIATION_H
 
+#include <algorithm>
+#include <cassert>
 #include <stdexcept>
+#include <tuple>
 #include <libbear/core/random.h>
 #include <libbear/ea/elements.h>
 #include <libbear/ea/genotype.h>
@@ -65,32 +68,87 @@ namespace libbear {
     const recombination_fn recombine_;
   };
   
-  template<typename T>
-  class iterative_recombination {
-    using fn = std::function<T(T, T)>;
+  namespace detail {
+
+    template<typename T>
+    class iterative_recombination_on_range {
+    public:
+      using fn = std::function<std::tuple<T, T>(T, T)>;
+      using arg_t = std::tuple<fn, std::size_t, std::size_t>;
+
+    public:
+      explicit iterative_recombination_on_range(const arg_t& arg)
+        : f_{std::get<0>(arg)}
+        , start_{std::get<1>(arg)}
+        , length_{std::get<2>(arg)}
+      {}
+
+      void operator()(genotype& g0, genotype& g1) const {
+        if (std::min(g0.size(), g1.size()) < start_ + length_) {
+          throw
+            std::logic_error{"iterative_recombination_on_range: size mismatch"};
+        }
+        for (std::size_t i = start_; i < start_ + length_; ++i) {
+          const auto [a, b] = f_(g0[i]->value<T>(), g1[i]->value<T>());
+          g0[i]->value<T>(a);
+          g1[i]->value<T>(b);
+        }
+      }
+
+    private:
+      fn f_;
+      std::size_t start_;
+      std::size_t length_;
+    };
+
+  } // namespace detail
+
+  template<std::size_t N, typename... Ts>
+  class iterative_recombination
+    : public detail::iterative_recombination_on_range<Ts>... {
+    static_assert(N == 1 || N == 2);
+
+    template<typename T>
+    using base = detail::iterative_recombination_on_range<T>;
 
   public:
-    explicit iterative_recombination(const fn& f) : f_{f} {}
+    template<typename T>
+    using fn = std::function<std::tuple<T, T>(T, T)>;
+
+    template<typename T>
+    using arg_t = std::tuple<fn<T>, std::size_t, std::size_t>;
+
+  public:
+    explicit(sizeof...(Ts) == 1)
+    iterative_recombination(const arg_t<Ts>&... args) : base<Ts>{args}...
+    {}
 
     population operator()(const genotype& g0, const genotype& g1) const {
       if (g0.size() != g1.size()) {
         throw std::logic_error{"iterative_recombination: size mismatch"};
       }
-      genotype g_res{g0};
-      for (std::size_t i = 0; i < g0.size(); ++i) {
-        g_res[i]->value<T>(f_(g0[i]->value<T>(), g1[i]->value<T>()));
+      genotype g0_res{g0};
+      genotype g1_res{g1};
+      (base<Ts>::operator()(g0_res, g1_res), ...);
+      if (N == 1) {
+        assert(g0_res == g1_res);
+        return population{g0_res};
+      } else {
+        return population{g0_res, g1_res};
       }
-      return population{g_res};
     }
-
-  private:
-    fn f_;
   };
 
   template<typename T>
   population arithmetic_recombination(const genotype& g0, const genotype& g1) {
-    return iterative_recombination<T>{
-      [](T a, T b) { return 0.5 * a + 0.5 * b; }
+    using type = typename iterative_recombination<1, T>::arg_t<T>;
+    return iterative_recombination<1, T>{
+      type{[](T a, T b) {
+        const T res = 0.5 * a + 0.5 * b;
+        return std::tuple<T, T>{res, res};
+      },
+      0,
+      g0.size()}
     }(g0, g1);
   }
 
