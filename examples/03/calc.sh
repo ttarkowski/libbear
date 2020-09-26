888422e4fb411809851242233ceb3c843285ec95
Tomasz Tarkowski <Tomasz.Tarkowski@fuw.edu.pl>
2020-09-26 21:53:25 +0200

    Fix.

--- examples/03/calc.sh
+++ examples/03/calc.sh
@@ -3,11 +3,21 @@
 # Arguments
 # 1 - pw.x input file
 
+error_msg="Calculations failed."
+
 pw.x < $1 >& ${1}.out
-result=`cat ${1}.out | grep "JOB DONE" | wc -l`
-if [[ ${result} == 1 ]]; then
+
+stop=`cat ${1}.out | grep "STOP" | wc -l`
+if [ ${stop} -gt 0 ]; then
+  echo ${error_msg}
+  exit
+fi
+
+job_done=`cat ${1}.out | grep "JOB DONE" | wc -l`
+if [[ ${job_done} == 1 ]]; then
   cat ${1}.out | grep "!" | grep "total energy" | tail -n 1 | awk '{print $5}'
 else
-  echo Calculations failed.
+  echo ${error_msg}
 fi
+exit
 
